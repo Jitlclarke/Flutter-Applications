e517541ac2cad7d38744bb2c90b0e0a78a31a95f
Justin Clarke <ji_tlc@hotmail.com>
2023-05-28 16:39:34 -0400

    updates
    push for main dart file and added constructors

---
+++ first_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        ///backgroundColor: Color.fromARGB(255, 187, 197, 249),
+        body: GradientContainer(Color.fromARGB(255, 3, 172, 244), Color.fromARGB(255, 158, 4, 175)),
+      ),
+    ),
+  );
+}
+
